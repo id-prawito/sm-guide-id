4763f40fc0eb858d449071b9cd3221e4bd2a5b65
Prawito <prawito.id@gmail.com>
2022-01-16 21:36:14 +0700

    setup-project

--- src/app/App.js
+++ src/app/App.js
@@ -3,7 +3,10 @@ import "./app.css";
 function App() {
     return (
         <div className="App">
-            <h1>Hello World</h1>
+            <h1>
+                Hello World chart-js react-router-dom react-circular-progressbar
+                react-router-dom react-chartjs-2 sass styled-components
+            </h1>
         </div>
     );
 }
